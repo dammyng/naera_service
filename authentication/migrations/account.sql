c1a58811be8e8401d574aaa925f8c4c9f6e22a9d
kdnotes <eidetic@outlook.com>
2021-04-12 14:05:49 +0100

    integration test in staging

--- authentication/migrations/account.sql
+++ authentication/migrations/account.sql
@@ -28,7 +28,7 @@ CREATE TABLE `accounts` (
   `bvn` longtext,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 INSERT INTO
   `accounts` (
     `id`,
