c782480012a19f5090b00fbae0186616348d7d39
kd <eidetic@outlook.com>
2021-04-14 05:38:37 +0100

    grpc server and client

--- authentication/naeragrpc/client.go
+++ authentication/naeragrpc/client.go
@@ -4,6 +4,7 @@ import (
 	"authentication/models/v1"
 	"context"
 
+	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
 )
 
@@ -32,4 +33,23 @@ func (c *naeraServiceClient) RegisterAccount(ctx context.Context, in *models.Acc
 		return nil, err
 	}
 	return result, err
+}
+
+
+func (c *naeraServiceClient) FindAccount(ctx context.Context, in *models.Account, opts ...grpc.CallOption) (*models.Account, error) {
+	ss := models.NewNaeraServiceClient(c.Conn)
+	result, err := ss.FindAccount(ctx, in, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return result, err
+}
+
+func (c *naeraServiceClient) UpdateAccount(ctx context.Context, in *models.UpdateAccountRequest, opts ...grpc.CallOption) (*empty.Empty, error) {
+	out := new(empty.Empty)
+	err := c.Conn.Invoke(ctx, "/models.NaeraService/UpdateAccount", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
 }
