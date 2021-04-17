fe4b85c71d03210947b9e5e6d0b8a4e885779c16
kd <eidetic@outlook.com>
2021-04-17 23:06:40 +0100

    utility functions

---
+++ authentication/pkg/helpers/utils.go
@@ -0,0 +1,28 @@
+package helpers
+
+import (
+	"authentication/models/v1"
+	"net/http"
+)
+
+func SetupCors(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+
+	if req.Method == "OPTIONS" {
+		(*w).Header().Set("Access-Control-Max-Age", "1728000")
+		(*w).Header().Set("Response-Code", "204")
+	}
+
+	(*w).Header().Set("Access-Control-Allow-Methods", "*")
+	(*w).Header().Set("Access-Control-Allow-Headers", "*")
+}
+
+func AccountReady(a *models.Account)  bool {
+	if a.EmailVerifiedAt < 1000  || a.PhoneVerifiedAt < 1000 ||  a.BvnVerifiedAt < 1000 ||  a.NubanVerifiedAt < 1000 {
+		return false
+	}
+	if a.IdCard == "" {
+		return false
+	}
+	return true
+}
