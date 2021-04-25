4736a0480a5151af2317faacd44998c9e80d3a3c
kd <eidetic@outlook.com>
2021-04-25 04:57:15 +0100

    moving from mock to live DB

--- bills/internals/db/store.go
+++
@@ -1,9 +0,0 @@
-package db
-
-import "bills/internals/models/v1"
-
-var categories = []models.DisplayCategory{
-	{Title: "Airtime", CreatedOn: "", IsActive: true}, 
-	{Title: "Data plan", CreatedOn: "", IsActive: true}, 
-	{Title: "Vehicle licence", CreatedOn: "", IsActive: false},
-}
--- bills/internals/models/v1/models.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type DisplayCategory struct {
-	Title     string `json:"title"`
-	CreatedOn string `json:"created_on"`
-	IsActive  bool   `json:"is_active"`
-}
